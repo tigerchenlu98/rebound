0aa86690cebca5a18987d621c74ab9fcdf56f53b
Hanno Rein <hanno@hanno-rein.de>
2021-06-26 16:39:36 -0400

    BS variables

--- src/rebound.h
+++ src/rebound.h
@@ -217,6 +217,25 @@ struct reb_simulation_integrator_whfast {
 };
 
 struct reb_simulation_integrator_bs {
+    int maxOrder; 
+    int* sequence;      // stepsize sequence
+    int* costPerStep;   // overall cost of applying step reduction up to iteration k + 1, in number of calls.
+    double* costPerTimeUnit; // cost per unit step.
+    double* optimalStep; // optimal steps for each order. 
+    double** coeffl;    // extrapolation coefficients.
+    int performTest; // stability check enabling parameter
+    int maxChecks;  // maximal number of checks for each iteration. 
+    int maxIter;    // maximal number of iterations for which checks are performed.
+    double stabilityReduction; // stepsize reduction factor in case of stability check failure.
+    double stepControl1; 
+    double stepControl2; 
+    double stepControl3; 
+    double stepControl4; 
+    double orderControl1;
+    double orderControl2;
+    int useInterpolationError; // use interpolation error in stepsize control.
+    int mudif; // interpolation order control parameter.
+
 };
 
 enum REB_EOS_TYPE {
