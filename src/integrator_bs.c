aed587247f5ad2d58cb3e784cd21875cfe7492bc
Hanno Rein <hanno@hanno-rein.de>
2021-06-26 16:27:20 -0400

    Copyright

--- src/integrator_bs.c
+++ src/integrator_bs.c
@@ -3,7 +3,10 @@
  * @brief 	BS integration scheme.
  * @author 	Hanno Rein <hanno@hanno-rein.de>
  * @details	This file implements the Gragg-Bulirsch-Stoer integration scheme.  
- 
+ *          It is a reimplementation of the fortran code by E. Hairer and G. Wanner.
+ *          The starting point was the JAVA implementation in hipparchus:
+ *          https://github.com/Hipparchus-Math/hipparchus/blob/master/hipparchus-ode/src/main/java/org/hipparchus/ode/nonstiff/GraggBulirschStoerIntegrator.java
+ *
  * @section 	LICENSE
  * Copyright (c) 2021 Hanno Rein
  *
@@ -21,6 +24,30 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2004, Ernst Hairer
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ * 
+ *  - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
 
