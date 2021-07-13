e96251e7fb78a50fce01c15410ec2ee5a8d91537
Hanno Rein <hanno@hanno-rein.de>
2021-07-13 19:16:06 -0400

    example

--- examples/bs_test/problem.c
+++ examples/bs_test/problem.c
@@ -14,22 +14,22 @@ int main(int argc, char* argv[]) {
 
     reb_add_fmt(r, "m", 1.);                // Central object
     reb_add_fmt(r, "m a e", 1e-3, 1., 0.1); // Jupiter mass planet
+    reb_move_to_com(r);
     double E0 = reb_tools_energy(r);
 
     r->integrator = REB_INTEGRATOR_BS;
+    r->ri_bs.scalAbsoluteTolerance = 1e-14;
+    r->ri_bs.scalRelativeTolerance = 1e-14;
     r->dt = 1e-4;
 
-    printf("==============================\n");
-    reb_step(r);
-    printf("==============================\n");
-    reb_step(r);
-    printf("==============================\n");
-    reb_step(r);
-    printf("==============================\n");
+    for(int i=0; i<100;i ++){
+        printf("==============================\n");
+        reb_step(r);
+        double E1 = reb_tools_energy(r);
+        printf("t  = %20.16f     DE/E = %.3e\n",r->t, fabs((E1-E0)/E0));
+        printf("dt = %20.16f  \n",r->dt);
+    }
 
-    double E1 = reb_tools_energy(r);
-    printf("t  = %20.16f     DE/E = %.3e\n",r->t, fabs((E1-E0)/E0));
-    printf("dt = %20.16f  \n",r->dt);
     reb_free_simulation(r);
 }
 
